22005142b664c52811990f48578cc7ea70b31a5a
Bohdan Hryshchenko <bgr11n@gmail.com>
2015-02-20 18:25:33 +0200

    Sending request with httparty to api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require knockout
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
 
+  root to: 'mood#index'
+  get '/load_tracks', to: 'mood#load_tracks'
+
 end
